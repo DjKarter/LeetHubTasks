563bc74a2c15220c55369a03b41e991277fa9404
Dmitrii <43564888+DjKarter@users.noreply.github.com>
2023-08-03 20:52:13 +0300

    Time: 117 ms (87%), Space: 48.1 MB (31.76%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+let romanToInt = function(s) {
+      const dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    if (s.length === 1) {
+        return dict[s[0]];
+    }
+    let i = 0;
+
+    let ans = 0;
+    while(i !== s.length - 1) {
+        if (dict[s[i]] >= dict[s[i + 1]]) {
+            ans += dict[s[i]];
+        } else {
+            ans -= dict[s[i]];
+        }
+        ++i;
+    }
+
+    return ans + dict[s[s.length - 1]];
+};
