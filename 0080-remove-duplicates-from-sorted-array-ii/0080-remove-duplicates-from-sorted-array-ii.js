c91b744dc00dda7f4d33c586d51fb58eaaa15893
Dmitrii <43564888+DjKarter@users.noreply.github.com>
2023-08-11 20:11:43 +0300

    Time: 68 ms (55.21%), Space: 44.4 MB (25.41%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -4,6 +4,7 @@
  */
 var removeDuplicates = function(nums) {
     let iter = 0, count = 0; 
+    // if count = 0 then we found only 1 number, if count = 1, then 2 numbers
     for (let i = 1; i < nums.length; ++i) {
         if (nums[i] !== nums[iter]) {
             nums[++iter] = nums[i];
