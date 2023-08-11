0e771d80248db0cdb8e4a7e390a97634d5005a67
Dmitrii <43564888+DjKarter@users.noreply.github.com>
2023-08-11 20:06:06 +0300

    Time: 59 ms (91.92%), Space: 44.7 MB (54.04%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,11 +3,6 @@
  * @return {number}
  */
 let removeDuplicates = function(nums) {
-    let swap = (i, j) => {
-        let temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
     let iter = 0; 
     for (let i = 1; i < nums.length; ++i) {
         if (nums[i] !== nums[iter]) 
