8b599cd1a03d703d7b8fe9dcba06c1b70ce5c8bf
Dmitrii <43564888+DjKarter@users.noreply.github.com>
2023-08-22 20:32:05 +0300

    Time: 53 ms (66.43%), Space: 41.4 MB (90.73%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -3,4 +3,11 @@
  * @param {Function} fn
  * @return {number[]}
  */
-let filter = (arr, fn) => arr.filter(fn)
+let filter = (arr, fn) => {
+  let ans = [];
+  for (let i = 0; i < arr.length; ++i) {
+    if (fn(arr[i], i))
+      ans.push(arr[i]);
+  }
+  return ans;
+}
